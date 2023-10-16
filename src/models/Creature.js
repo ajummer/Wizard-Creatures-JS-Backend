01d2656e25ff9b30de0c272d1f2e010e3a4da995
ajummer <makeudreamempty@gmail.com>
2023-10-16 15:47:01 +0300

    Add creature model

---
+++ src/models/Creature.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const creatureSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  species: {
+    type: String,
+    required: true,
+  },
+  skinColor: {
+    type: String,
+    required: true,
+  },
+  eyeColor: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+const Creature = mongoose.model("Creature", creatureSchema);
+
+module.exports = Creature;
