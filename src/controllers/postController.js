124f2081ec3893be7c6107bc544ec5066bdd09e5
ajummer <makeudreamempty@gmail.com>
2023-10-16 16:03:15 +0300

    Add create post functionality

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,10 +1,26 @@
 const router = require("express").Router();
+const creatureService = require("../services/creatureService.js");
+const { getErrorMessage } = require("../utils/errorHelpers.js");
 
 router.get("/", (req, res) => {
   res.render("posts");
 });
 
-router.get("/create" , (req,res) =>{
-  res.render("posts/create")
-})
+router.get("/create", (req, res) => {
+  res.render("posts/create");
+});
+
+router.post("/create", async (req, res) => {
+  const creatureData = {
+    ...req.body,
+    owner: req.user._id,
+  };
+  console.log(creatureData)
+  try {
+    await creatureService.create(creatureData);
+    res.redirect("/posts");
+  } catch (err) {
+    res.render("posts/create", { error: getErrorMessage(err) });
+  }
+});
 module.exports = router;
